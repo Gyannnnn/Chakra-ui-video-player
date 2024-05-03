72b06ab1de16dcc1b3b315d54262ffd83bd83a5f
Gyanranjan Patra <hi.gyanaranjanpatra@gmail.com>
2024-05-03 17:11:34 +0530

    .

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import {BrowserRouter as Router,Route,Routes} from "react-router-dom"
+import Header from "./Components/Header";
+import Home from "./Components/Home";
+
+function App() {
+  return (
+    <Router>
+      <Header/>
+      <Routes>
+        <Route path="/"element={<Home/>} />
+      </Routes>
+    </Router>
+    
+  );
+}
+
+export default App;
