4f8027c741382a8dd4420fd68dcb70286dd80813
Gyanranjan Patra <hi.gyanaranjanpatra@gmail.com>
2024-05-04 12:17:07 +0530

    updated

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -8,6 +8,17 @@ import img2 from '../Assets/2.jpg'
 import img3 from '../Assets/3 (1).jpg'
 import img4 from '../Assets/4 (1).jpg'
 import img5 from '../Assets/5.png'
+import { transform } from 'framer-motion';
+
+
+const headingOptions = {
+  pos: 'absolute',
+  left: '50%',
+  top: '50%',
+  transform: 'translate(-50% ,-50%)',
+  textTransformation: 'uppercase',
+  padding: '4',
+}
 
 const Home = () => {
   return (
@@ -21,21 +32,41 @@ const Home = () => {
 const MyCarousel = () => {
 
   return (
-    <Carousel autoPlay infiniteLoop interval={1000} showStatus={true} showIndicators={true} showThumbs={true} showArrows={true} >
-      <Box  w={'full'} h={'100vh'}  >
-        <Image src={img1} />
-        <Heading bgColor={'black'} color={'white'} >
-          Watch The Future
+
+    <Carousel autoPlay infiniteLoop interval={1000} >
+      <Box w='full'  >
+        <Image src={img1} ></Image>
+        <Heading bg={'black'} color={'white'}  {...headingOptions} >
+          watch The Future
         </Heading>
+
       </Box>
-      <Box w={'full'} h={'100vh'} >
-        <Image src={img2} />
-        <Heading bgColor={'black'} color={'white'} >
-        Gaming IS The Future
+      <Box>
+        <Image src={img2} ></Image>
+        <Heading bg={'black'} color={'white'} >
+          Future Is Gaming
         </Heading>
+
+      </Box>
+      <Box>
+        <Image src={img3} ></Image>
+        <Heading bg={'black'} color={'white'} >
+          Night Life Is cool
+        </Heading>
+
+      </Box>
+      <Box>
+        <Image src={img4} ></Image>
+        <Heading bg={'black'} color={'white'} >
+          Future Is Gaming
+        </Heading>
+
       </Box>
 
+
+
     </Carousel>
+
   )
 }
 
